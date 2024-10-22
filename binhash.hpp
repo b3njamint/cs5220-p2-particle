4d2c9940cc1608362db0d5557a78ef5bc5386a52
David Han <dmh338@login40.chn.perlmutter.nersc.gov>
2024-10-21 19:56:37 -0700

    change makefile and lock

--- binhash.hpp
+++ binhash.hpp
@@ -2,6 +2,8 @@
 #define BINHASH_H
 
 #include "state.hpp"
+#include <vector>
+#include <omp.h>
 
 /*@T
  * \section{Spatial hashing}
@@ -46,6 +48,7 @@
 unsigned particle_bucket(particle_t* p, float h);
 unsigned particle_neighborhood(unsigned* buckets, particle_t* p, float h);
 void hash_particles(sim_state_t* s, float h);
+std::vector<omp_lock_t> create_locks(int n);
 
 /*@q*/
 #endif /* BINHASH_H */
