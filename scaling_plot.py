c038540b3495560d5e740bdf04c86092e4bad5bd
Eugene Choi <4eugenechoi@gmail.com>
2024-10-23 21:44:06 -0700

    parallel log log plot

--- scaling_plot.py
+++ scaling_plot.py
@@ -28,16 +28,16 @@ def plot_core(data):
     plt.show()
 
 def plot_particle(data):
-    h = list(data.keys())
+    N = list(data.keys())
     times = list(data.values())
 
     plt.figure(figsize=(10, 6))
-    plt.loglog(h, times, marker='o')
-    plt.title('Log-Log Execution Time vs. Particle Size')
-    plt.xlabel('Log Particle Size (h)')
+    plt.loglog(N, times, marker='o')
+    plt.title('Log-Log Number of Particles vs. Execution Time (Parallel)')
+    plt.xlabel('Log Number of Particles')
     plt.ylabel('Log Execution Time (seconds)')
     plt.grid(True)
-    plt.xticks(h)
+    plt.xticks(N)
     plt.savefig('scaling_plot_particles.png')
     plt.show()
 
@@ -57,14 +57,19 @@ def main():
 
     # plot_core(data)
 
-    char_to_h = {c : h for c, h in zip(list(string.ascii_lowercase[:9]), [5, 2, 1, 5e-1, 2e-1, 1e-1, 5e-2, 2e-2, 1e-2, 1e-3])}
+    #char_to_h = {c : h for c, h in zip(list(string.ascii_lowercase[1:8]), [7e-1, 5e-1, 3e-1, 2e-1, 1e-1, 8e-2, 7e-2, 5e-2])}
+
+    char_to_h = {c : h for c, h in zip(list(string.ascii_lowercase[:7]), [ 0.17, 0.15, 0.13, 0.11, 0.09, 0.07, 0.05])}
 
     for c in list(string.ascii_lowercase[:7]):
-        out_file = f'sph_outputs_particles/sph_{c}.out'
+        out_file = f"sph_outputs_particles/sph_{c}.out"
         if os.path.exists(out_file):
             time = get_time(out_file)
             if time is not None:
-                data[char_to_h[c]] = time
+                vol_box = 1.0  # 1x1x1 box
+                vol_particle = char_to_h[c] ** 3
+                N = int(vol_box / vol_particle)
+                data[N] = time
             else:
                 print(f"Could not get time from {out_file}")
         else:
